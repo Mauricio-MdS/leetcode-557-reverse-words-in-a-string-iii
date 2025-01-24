66e80418c990c66d0a9b113f6122056c57df53e9
Mauricio-MdS <45921381+Mauricio-MdS@users.noreply.github.com>
2025-01-24 10:37:32 -0300

    Refactor to improve performance

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,19 +1,12 @@
 class Solution {
     public String reverseWords(String s) {
-        StringBuilder result = new StringBuilder();
-        StringBuilder word = new StringBuilder();
+        String[] words = s.split(" ");
+        StringBuilder reversedWords = new StringBuilder();
 
-        for (char c : s.toCharArray()) {
-            if (c == ' ') {
-                result.append(word.reverse());
-                word.setLength(0);
-                result.append(' ');
-            } else {
-                word.append(c);
-            }
+        for (String word : words) {
+            reversedWords.append(new StringBuilder(word).reverse().append(" "));
         }
 
-        result.append(word.reverse());
-        return result.toString();
+        return reversedWords.deleteCharAt(reversedWords.length() -1).toString();
     }
 }
